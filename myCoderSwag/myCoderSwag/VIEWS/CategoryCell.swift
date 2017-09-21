9804c42850335d312329c5d04cb1c876a1b60027
Gabriel Abraham <gabrielabraham@Gabriels-Mac-mini.local>
2017-09-21 10:38:32 +0100

    Feeding the table view with data

---
+++ myCoderSwag/myCoderSwag/VIEWS/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  myCoderSwag
+//
+//  Created by Gabriel Abraham on 21/09/2017.
+//  Copyright © 2017 Gabriel Abraham. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
